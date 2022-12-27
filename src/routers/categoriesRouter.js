e17e00c77ab83214b20524e93561202e989be409
NicWirnt <97414933+NicWirnt@users.noreply.github.com>
2022-12-28 10:00:47 +1100

    categories router update

--- src/routers/categoriesRouter.js
+++ src/routers/categoriesRouter.js
@@ -4,8 +4,9 @@ import { getAllCategories } from "../models/category/Category.models.js";
 const router = express.Router();
 
 //return all active categories
-router.get("/", async (req, res, next) => {
+router.get("/:_id?", async (req, res, next) => {
   try {
+    const { _id } = req.params;
     const filter = { status: "active" };
     const result = await getAllCategories(filter);
 
