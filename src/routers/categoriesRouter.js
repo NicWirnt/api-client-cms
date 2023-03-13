7d52328867b6deaf5d10f7ffc99a2d6d29c15c19
NicWirnt <97414933+NicWirnt@users.noreply.github.com>
2023-03-13 13:39:31 +1100

    cart done

--- src/routers/categoriesRouter.js
+++ src/routers/categoriesRouter.js
@@ -1,5 +1,10 @@
 import express from "express";
-import { getAllCategories } from "../models/category/Category.models.js";
+import {
+  getAllCategories,
+  getCategories,
+  getCategory,
+} from "../models/category/Category.models.js";
+import { getMultipleProducts } from "../models/product/Product.model.js";
 
 const router = express.Router();
 
@@ -8,9 +13,22 @@ router.get("/:_id?", async (req, res, next) => {
   try {
     const { _id } = req.params;
     const filter = { status: "active" };
-    const result = await getAllCategories(filter);
 
-    res.json({
+    const result = _id
+      ? await getMultipleProducts({ status: "active", catId: _id })
+      : await getCategories(filter);
+
+    if (_id) {
+      const cat = await getCategory({ status: "active", _id });
+      const { catName } = cat;
+      return res.json({
+        status: "success",
+        message: "Categories result",
+        result,
+        catName,
+      });
+    }
+    return res.json({
       status: "success",
       message: "Categories result",
       result,
